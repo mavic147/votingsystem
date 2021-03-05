42a2693217e92389c82fdbf46fab0b2455934dbe
mavic147 <vamaximenko104@gmail.com>
2021-03-05 13:33:52 +0300

    Spring Container configured Db connection configured

--- src/main/java/ru/maximenko/votingsystem/model/Restaurant.java
+++ src/main/java/ru/maximenko/votingsystem/model/Restaurant.java
@@ -2,7 +2,7 @@ package ru.maximenko.votingsystem.model;
 
 public class Restaurant extends AbstractNamedEntity {
 
-    public Restaurant(Integer id, String name, Integer rating) {
+    public Restaurant(Integer id, String name) {
         super(id, name);
     }
 
