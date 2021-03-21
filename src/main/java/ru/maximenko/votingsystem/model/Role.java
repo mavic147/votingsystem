fc869b9931d49fd209e4478bc8fc1cc1dc33cafc
mavic147 <vamaximenko104@gmail.com>
2021-03-21 18:44:04 +0300

    Enum Role fixed

--- src/main/java/ru/maximenko/votingsystem/model/Role.java
+++ src/main/java/ru/maximenko/votingsystem/model/Role.java
@@ -2,10 +2,5 @@ package ru.maximenko.votingsystem.model;
 
 public enum Role {
     USER,
-    ADMIN;
-//    int roleId;
-//
-//    Role(int roleId) {
-//        this.roleId = roleId;
-//    }
+    ADMIN
 }
