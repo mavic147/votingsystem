226c068f7ed3d0fcbd34566e2366c6b63f386642
mavic147 <vamaximenko104@gmail.com>
2021-04-02 11:12:01 +0300

    MenuMockData refactored

--- src/test/java/ru/maximenko/votingsystem/service/MenuServiceTest.java
+++ src/test/java/ru/maximenko/votingsystem/service/MenuServiceTest.java
@@ -20,13 +20,13 @@ public class MenuServiceTest extends AbstractServiceTest {
 
     @Test
     public void getOne() {
-        Menu actual = menuService.get(MENU1_ID, RESTAURANT1_ID);
-        assertThat(actual).usingRecursiveComparison().ignoringFields("dateChange").isEqualTo(menu1);
+        Menu actual = menuService.get(ITEM1_ID, RESTAURANT1_ID);
+        assertThat(actual).usingRecursiveComparison().ignoringFields("dateChange").isEqualTo(item1);
     }
 
     @Test
     public void getNotOwn() {
-        assertThrows(EntityNotFoundException.class, () -> menuService.get(MENU1_ID, RESTAURANT2_ID));
+        assertThrows(EntityNotFoundException.class, () -> menuService.get(ITEM1_ID, RESTAURANT2_ID));
     }
 
     @Test
@@ -48,32 +48,32 @@ public class MenuServiceTest extends AbstractServiceTest {
     @Test
     public void update() {
         menuService.update(getUpdated(), RESTAURANT1_ID);
-        assertThat(menuService.get(MENU2_ID, RESTAURANT1_ID)).usingRecursiveComparison().ignoringFields("dateChange").isEqualTo(getUpdated());
+        assertThat(menuService.get(ITEM2_ID, RESTAURANT1_ID)).usingRecursiveComparison().ignoringFields("dateChange").isEqualTo(getUpdated());
     }
 
     @Test
     public void updateNotOwn() {
         EntityNotFoundException ex = assertThrows(EntityNotFoundException.class, () -> menuService.update(getUpdated(), RESTAURANT2_ID));
-        assertEquals("Not found entity with id = " + (MENU2_ID), ex.getMessage());
-        assertThat(menuService.get(MENU2_ID, RESTAURANT1_ID)).usingRecursiveComparison().ignoringFields("dateChange").isEqualTo(menu2);
+        assertEquals("Not found entity with id = " + (ITEM2_ID), ex.getMessage());
+        assertThat(menuService.get(ITEM2_ID, RESTAURANT1_ID)).usingRecursiveComparison().ignoringFields("dateChange").isEqualTo(item2);
     }
 
     @Test
     public void delete() {
-        menuService.delete(MENU7_ID, RESTAURANT2_ID);
-        assertThrows(EntityNotFoundException.class, () -> menuService.get(MENU7_ID, RESTAURANT2_ID));
+        menuService.delete(ITEM7_ID, RESTAURANT2_ID);
+        assertThrows(EntityNotFoundException.class, () -> menuService.get(ITEM7_ID, RESTAURANT2_ID));
     }
 
     @Test
     public void deleteNotFound() {
-        menuService.delete(MENU6_ID, RESTAURANT2_ID);
-        assertThrows(EntityNotFoundException.class, () -> menuService.delete(MENU6_ID, RESTAURANT2_ID));
+        menuService.delete(ITEM6_ID, RESTAURANT2_ID);
+        assertThrows(EntityNotFoundException.class, () -> menuService.delete(ITEM6_ID, RESTAURANT2_ID));
         assertThrows(EntityNotFoundException.class, () -> menuService.delete(NOT_FOUND, RESTAURANT2_ID));
     }
 
     @Test
     public void deleteNotOwn() {
-        assertThrows(EntityNotFoundException.class, () -> menuService.delete(MENU1_ID, RESTAURANT2_ID));
+        assertThrows(EntityNotFoundException.class, () -> menuService.delete(ITEM1_ID, RESTAURANT2_ID));
     }
 
 }
--- src/test/java/ru/maximenko/votingsystem/util/MenuMockData.java
+++ src/test/java/ru/maximenko/votingsystem/util/MenuMockData.java
@@ -12,41 +12,40 @@ import static ru.maximenko.votingsystem.util.DishMockData.*;
 
 public class MenuMockData {
 
-    public static final int MENU1_ID = START_SEQ + 15;
-    public static final int MENU2_ID = MENU1_ID + 1;
-    public static final int MENU3_ID = MENU2_ID + 1;
-    public static final int MENU4_ID = MENU3_ID + 1;
-    public static final int MENU5_ID = MENU4_ID + 1;
-    public static final int MENU6_ID = MENU5_ID + 1;
-    public static final int MENU7_ID = MENU6_ID + 1;
+    public static final int ITEM1_ID = START_SEQ + 15;
+    public static final int ITEM2_ID = ITEM1_ID + 1;
+    public static final int ITEM3_ID = ITEM2_ID + 1;
+    public static final int ITEM4_ID = ITEM3_ID + 1;
+    public static final int ITEM5_ID = ITEM4_ID + 1;
+    public static final int ITEM6_ID = ITEM5_ID + 1;
+    public static final int ITEM7_ID = ITEM6_ID + 1;
     public static final int NOT_FOUND = START_SEQ + 750;
 
-    public static final Menu menu1 = new Menu(MENU1_ID, RESTAURANT1_ID, DISH1_ID, 600d,
+    public static final Menu item1 = new Menu(ITEM1_ID, RESTAURANT1_ID, DISH1_ID, 600d,
             LocalDate.now());
-    public static final Menu menu2 = new Menu(MENU2_ID, RESTAURANT1_ID, DISH2_ID, 580d,
+    public static final Menu item2 = new Menu(ITEM2_ID, RESTAURANT1_ID, DISH2_ID, 580d,
             LocalDate.now());
-    public static final Menu menu3 = new Menu(MENU3_ID, RESTAURANT1_ID, DISH3_ID, 340d,
+    public static final Menu item3 = new Menu(ITEM3_ID, RESTAURANT1_ID, DISH3_ID, 340d,
             LocalDate.now());
-    public static final Menu menu4 = new Menu(MENU4_ID, RESTAURANT2_ID, DISH4_ID,
+    public static final Menu item4 = new Menu(ITEM4_ID, RESTAURANT2_ID, DISH4_ID,
             368d, LocalDate.now());
-    public static final Menu menu5 = new Menu(MENU5_ID, RESTAURANT2_ID, DISH5_ID,
+    public static final Menu item5 = new Menu(ITEM5_ID, RESTAURANT2_ID, DISH5_ID,
             150d, LocalDate.now());
-    public static final Menu menu6 = new Menu(MENU6_ID, RESTAURANT2_ID, DISH6_ID,
+    public static final Menu item6 = new Menu(ITEM6_ID, RESTAURANT2_ID, DISH6_ID,
             315d, LocalDate.now());
-    public static final Menu menu7 = new Menu(MENU7_ID, RESTAURANT2_ID, DISH7_ID,
+    public static final Menu item7 = new Menu(ITEM7_ID, RESTAURANT2_ID, DISH7_ID,
             165d, LocalDate.now());
 
-    public static final List<Menu> rest1Menu = List.of(menu1, menu2, menu3);
-    public static final List<Menu> rest2Menu = List.of(menu4, menu5, menu6, menu7);
+    public static final List<Menu> rest1Menu = List.of(item1, item2, item3);
+    public static final List<Menu> rest2Menu = List.of(item4, item5, item6, item7);
 
     public static Menu getNew() {
         return new Menu(null, RESTAURANT1_ID, DISH4_ID, 390d, LocalDate.now());
     }
 
     public static Menu getUpdated() {
-        Menu updMenu = new Menu(menu2);
+        Menu updMenu = new Menu(item2);
         updMenu.setDishPrice(625d);
         return updMenu;
     }
-
 }
